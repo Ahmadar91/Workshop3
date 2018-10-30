81dcefc3deb734d27d28089b4020f81f2d8831f6
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2018-10-30 02:28:54 +0100

    v1

--- WorkShop 3/src/BlackJack/model/rules/AmericanNewGameStrategy.java
+++ WorkShop 3/src/BlackJack/model/rules/AmericanNewGameStrategy.java
@@ -3,36 +3,28 @@ package BlackJack.model.rules;
 import BlackJack.model.Deck;
 import BlackJack.model.Dealer;
 import BlackJack.model.Player;
-import BlackJack.model.Card;  
+import BlackJack.model.Card;
 
 class AmericanNewGameStrategy implements INewGameStrategy {
 
-  public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-    Card c = null;
-    for(int i= 0; i<2; i++) {
-    	dealPlayer(a_deck,a_player,c ); 
+	public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+		for (int i = 0; i < 2; i++) {
+			dealPlayer(a_deck, a_player);
+			dealDealer(a_deck, a_dealer);
+		}
+		return true;
+	}
 
-        dealPlayer( a_deck,  a_player,  c );
-    }
+	public void dealPlayer(Deck a_deck, Player a_player) {
+		Card c = a_deck.GetCard();
+		c.Show(true);
+		a_player.DealCard(c);
+	}
 
-    
+	public void dealDealer(Deck a_deck, Dealer a_dealer) {
+		Card c = a_deck.GetCard();
+		c.Show(true);
+		a_dealer.DealCard(c);
+	}
 
-  
-
-    return true;
-  }
-  
-  
-  public void dealPlayer(Deck a_deck, Player a_player, Card c ) {
-	  c = a_deck.GetCard();
-	   c.Show(true);
-	    a_player.DealCard(c);
-  }
-  
-  public void dealDealer(Deck a_deck, Dealer a_dealer, Card c ) {
-	  c= a_deck.GetCard();
-	   c.Show(true);
-	    a_dealer.DealCard(c);
-  }
-  
 }
